f61e190d16e8daf7352801e7b1e48273f3f61c8e
Maurice Schmid <maurice.schmid@live.de>
2020-02-12 23:18:23 -0800

    Update driver's log table

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -1,14 +1,76 @@
 import React, { Component } from "react";
 import { Table as SemanticTable } from "semantic-ui-react";
+import _ from "lodash";
+import PropTypes from "prop-types";
 
 class Table extends Component {
-  state = {};
-
   static Cell = SemanticTable.Cell;
 
+  renderHeader() {
+    const { columns, onSort } = this.props;
+
+    return columns.map(({ key, path, sortable, content }) => ({
+      key: key || path,
+      onClick:
+        sortable &&
+        function() {
+          onSort(path);
+        },
+      content
+    }));
+  }
+
+  renderBodyRow = (item, idx) => {
+    return {
+      key: item._id,
+      cells: this.renderBodyCells(item, idx)
+    };
+  };
+
+  renderBodyCells(item, idx) {
+    const { columns } = this.props;
+
+    return columns.map(column => ({
+      key: this.createKey(item, column),
+      content: this.getBodyContent(item, column, idx),
+      verticalAlign: column.verticalAlign || "middle"
+    }));
+  }
+
+  createKey(item, column) {
+    return item._id + (column.path || column.key);
+  }
+
+  getBodyContent(item, column, idx) {
+    const { counter, format, path, renderContent } = column;
+
+    if (counter) return idx + 1;
+    if (renderContent) return renderContent(item, column, idx);
+
+    let content = _.get(item, path);
+    if (format) content = format(content, { item, column, idx });
+
+    return content;
+  }
+
   render() {
-    return <SemanticTable {...this.props} />;
+    const props = { ...this.props };
+    delete props.columns;
+    delete props.onSort;
+
+    return (
+      <SemanticTable
+        headerRow={this.renderHeader()}
+        renderBodyRow={this.renderBodyRow}
+        tableData={props.data}
+        {...props}
+      />
+    );
   }
 }
+
+Table.propTypes = {
+  data: PropTypes.array.isRequired
+};
 
 export default Table;
---
+++ src/components/driversLogsTable.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import moment from "moment";
+import Table from "./common/table";
+import Button from "./common/button";
+
+class DriversLogsTable extends Component {
+  getColumns() {
+    const { onDelete } = this.props;
+
+    return [
+      { key: "counter", counter: true },
+      {
+        content: "Driver",
+        path: "driver",
+        format: function({ firstName, lastName }) {
+          return `${firstName} ${lastName}`;
+        },
+        sortable: true
+      },
+      {
+        content: "Desparture Date",
+        path: "departureDate",
+        format: this.formatDate,
+        sortable: true
+      },
+      {
+        content: "Arrival Date",
+        path: "arrivalDate",
+        format: this.formatDate,
+        sortable: true
+      },
+      {
+        content: "Departure Mileage",
+        path: "departureMileage",
+        sortable: true
+      },
+      { content: "Arrival Mileage", path: "arrivalMileage", sortable: true },
+      {
+        key: "tools",
+        renderContent: ({ _id }) => {
+          return (
+            <Button.Group>
+              <Button
+                icon="edit"
+                to={`/drivers-log/${_id}`}
+                basic
+                color="black"
+              />
+              <Button icon="delete" onClick={onDelete} basic color="red" />
+            </Button.Group>
+          );
+        }
+      }
+    ];
+  }
+
+  formatDate = content => {
+    const dateFormat = "DD MMM. YYYY HH:mm";
+
+    return moment(content).format(dateFormat);
+  };
+
+  render() {
+    const { data, onSort } = this.props;
+
+    return (
+      <Table
+        singleLine
+        sortable
+        striped
+        unstackable
+        definition
+        selectable
+        columns={this.getColumns()}
+        data={data}
+        onSort={onSort}
+      />
+    );
+  }
+}
+
+export default DriversLogsTable;
